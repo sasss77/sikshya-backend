bb23ad2b2637e004c678efbb7654c8a8442d710e
sasss77 <saugatpaneru40@gmail.com>
2026-07-17 19:30:01 +0545

    notigication repository added

---
+++ src/repositories/notification.repository.ts
@@ -0,0 +1,41 @@
+import { NotificationModel, NotificationType } from "../models/notification.model";
+import mongoose from "mongoose";
+
+export const createNotification = async (data: {
+  userId: string;
+  type: NotificationType;
+  title: string;
+  message: string;
+}) => {
+  return await NotificationModel.create({
+    ...data,
+    userId: new mongoose.Types.ObjectId(data.userId),
+  });
+};
+
+export const findNotificationsByUserId = async (userId: string) => {
+  return await NotificationModel.find({
+    userId: new mongoose.Types.ObjectId(userId),
+  }).sort({ createdAt: -1 });
+};
+
+export const markNotificationAsRead = async (notificationId: string) => {
+  return await NotificationModel.findByIdAndUpdate(
+    notificationId,
+    { $set: { read: true } },
+    { new: true }
+  );
+};
+
+export const markAllNotificationsAsRead = async (userId: string) => {
+  return await NotificationModel.updateMany(
+    { userId: new mongoose.Types.ObjectId(userId), read: false },
+    { $set: { read: true } }
+  );
+};
+
+export const clearAllNotifications = async (userId: string) => {
+  return await NotificationModel.deleteMany({
+    userId: new mongoose.Types.ObjectId(userId),
+  });
+};
